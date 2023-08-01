407e550f56173a85f0e0b73f5e74e1c60782f0f1
Chanhyeok Ko <chanhyeok@mondrian.ai>
2023-08-01 18:09:53 +0900

    20230801_addResource

--- DAG_creation/dag_API.py
+++ DAG_creation/dag_API.py
@@ -1,16 +1,22 @@
 from flask import Flask, request
 from jinja2 import Template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/create_dag', methods=['POST'])
 def create_dag():
     data = request.get_json()  # Get the JSON data sent with the POST request
+    # start_date를 API 요청 시간으로 설정
+    now=datetime.now()
+    data["default_arg"]["start_date"] = now.strftime('%Y, ') + str(now.month) + ', ' + str(now.day)
+    print(data["default_arg"]["start_date"])
+    
     template = Template(open('dag_template.j2').read())  # Load the Jinja2 template
     dag = template.render(data)  # Fill the template with the data
     print(data)
     # Write the filled template to a .py file in the Airflow DAGs directory
-    with open(f'/mnt/shared/airflow/dags/{data["dag"]["dag_id"]}.py', 'w') as f:
+    with open(f'/mnt/shared/airflow/dags/{data["default_arg"]["dag_owner"]}_{data["dag"]["dag_id"]}.py', 'w') as f:
         f.write(dag)
     
     return {'message': 'DAG created'}, 200
--- dags/c2h_Test_dynamic_dag.py
+++ dags/c2h_Test_dynamic_dag.py
@@ -6,6 +6,12 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
 )
 from kubernetes.client import models as k8s
 from airflow.models import Variable
+from kubernetes.client import V1ResourceRequirements
+
+pod_resources = V1ResourceRequirements(
+    requests={'memory': '128Mi', 'cpu': '100m'},
+    limits={'memory': '1Gi', 'cpu': '1'}
+)
 
 # Define volume
 volume = k8s.V1Volume(
@@ -24,16 +30,16 @@ mount = k8s.V1VolumeMount(
 default_args = {
     'owner': 'c2h',
     'depends_on_past': False,
-    'start_date': datetime.strptime('2023-07-24 00:00:00', '%Y-%m-%d %H:%M:%S'),
+    'start_date': datetime(2023, 8, 1),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': '1',
+    'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
-    'Test dynamic dag',
+    'Test_dynamic_dag',
     default_args=default_args,
     schedule_interval='@daily',
 )
@@ -42,16 +48,15 @@ def sleep_on_failure(context):
     print("Task failed. sleeping for 2minutes")
     time.sleep(120)
 
-
 test_task_1 = KubernetesPodOperator(
     namespace='airflow-cluster',
-    image="python:3.7",
-    cmds="['python', '-c']",
-    arguments="["print('hello'); x=1+1; print(x)"]",
-    name="task_1",
-    task_id="task_1",
+    image='python:3.7',
+    cmds=['python', '-c'],
+    arguments=["print('hello')", 'x=1+1', 'print(x)'],
+    name='task_1',
+    task_id='task_1',
     env_vars={},
-    resources={},
+    container_resources={},
     in_cluster=True,
     is_delete_operator_pod=True,
     on_failure_callback=sleep_on_failure,
@@ -59,18 +64,15 @@ test_task_1 = KubernetesPodOperator(
     dag=dag
 )
 
-
-
-
 test_task_2 = KubernetesPodOperator(
-    namespace='default',
-    image="python:3.7",
-    cmds="['python', '-c']",
-    arguments="["print('hello')", 'x=1+1', 'print(x)', "with open('/data/test_c2h.txt', 'w') as f: f.write(x)"]",
-    name="task_2",
-    task_id="task_2",
+    namespace='airflow-cluster',
+    image='python:3.7',
+    cmds=['python', '-c'],
+    arguments=["print('hello')", 'x=1+1', 'print(x)', "with open('/data/test_c2h.txt', 'w') as f: f.write(x)"],
+    name='task_2',
+    task_id='task_2',
     env_vars={'ENV_VAR1': 'value1', 'ENV_VAR2': 'value2'},
-    resources={},
+    container_resources={},
     in_cluster=True,
     is_delete_operator_pod=True,
     on_failure_callback=sleep_on_failure,
@@ -84,14 +86,14 @@ test_task_1 >> test_task_2
 
 
 test_task_3 = KubernetesPodOperator(
-    namespace='default',
-    image="python:3.7",
-    cmds="['python', '-c']",
-    arguments="["print('hello'); x=1+1; print(x);"]",
-    name="task_3",
-    task_id="task_3",
+    namespace='airflow-cluster',
+    image='python:3.7',
+    cmds=['python', '-c'],
+    arguments=["print('hello')"],
+    name='task_3',
+    task_id='task_3',
     env_vars={'ENV_VAR1': 'value1', 'ENV_VAR2': 'value2'},
-    resources={'limit_memory': '1G', 'limit_cpu': '1'},
+    container_resources=pod_resources,
     in_cluster=True,
     is_delete_operator_pod=True,
     on_failure_callback=sleep_on_failure,
